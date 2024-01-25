7447129bf7c45d12f59911e294c85959507c7478
Phil <philip.schaefer0@gmail.com>
2024-01-25 23:36:43 +0100

    fix error message on content replacer

--- functions/EVENT/interaction/isButton.js
+++ functions/EVENT/interaction/isButton.js
@@ -1,6 +1,5 @@
 module.exports.run = async (interaction) => {
   // await interaction.deferReply();
-  if (!interaction.member.roles.cache.has(config.teamRole)) return messageFail(interaction, 'Please wait for a Staffmember to verify you.\nYou can\'t use the buttons.');
   const func = client.functions.get(`ENGINE_${interaction.customId}`);
   if (func) return func.run(interaction).catch(ERR);
   // if (interaction.channelId === config.functions.checkin.outputChannels.todo) {
