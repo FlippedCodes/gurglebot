104a09989a506b8dd86e31ee8fa57162665c5d16
Phil <philip.schaefer0@gmail.com>
2024-03-15 22:55:47 +0100

    enable checkin command for everyone

--- commands/checkin.js
+++ commands/checkin.js
@@ -1,8 +1,8 @@
 module.exports.run = async (interaction) => {
   await interaction.deferReply();
 
-  // check if user is teammember
-  if (!interaction.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
+  // // check if user is teammember
+  // if (!interaction.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
 
   if (interaction.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a checkin channel.');
   await client.functions.get('ENGINE_checkin_postReaction').run(interaction);
