a0a587d63aa3baca86cd42da1cd0e056494f4ad5
Phil <philip.schaefer0@gmail.com>
2024-03-15 01:45:46 +0100

    fix no log

---
+++ commands/rp.js
@@ -0,0 +1,17 @@
+module.exports.run = async (interaction) => {
+  await interaction.deferReply({ ephemeral: true });
+
+  // only guild command
+  if (!await interaction.inGuild()) return messageFail(interaction, 'This command is for servers only.');
+  // check if user is owner
+  if (!interaction.member.roles.cache.find(({ id }) => id === config.ownerRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
+  // send message
+  const output = await interaction.channel.send(interaction.options.getString('message', true)).catch(ERR);
+  // send log and user confirmation
+  if (output) messageSuccess(interaction, 'Messages sent!', null, true);
+  else messageFail(interaction, 'Something went wrong... Do I have permissions to send in here?');
+};
+
+module.exports.data = new CmdBuilder()
+  .setName('rp')
+  .setDescription('See stats about your timeout.');
