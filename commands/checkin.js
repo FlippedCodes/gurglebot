2ee07655bfac6b95d19de8ff6bbe8da00fb3843a
Phil <philip.schaefer0@gmail.com>
2024-03-16 00:12:56 +0100

    added comment

--- commands/checkin.js
+++ commands/checkin.js
@@ -2,6 +2,7 @@ module.exports.run = async (interaction) => {
   await interaction.deferReply();
 
   // // check if user is teammember
+  // DISABLED: No need to check who asks for it
   // if (!interaction.member.roles.cache.find(({ id }) => id === config.teamRole)) return messageFail(interaction, 'You don\'t have access to this command! òwó');
 
   if (interaction.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a checkin channel.');
