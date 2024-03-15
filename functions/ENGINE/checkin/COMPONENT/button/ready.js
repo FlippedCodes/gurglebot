7538016f8824aef5ecbeb25faca66a7e87362e91
Phil <philip.schaefer0@gmail.com>
2024-03-15 23:27:53 +0100

    fixed ping

--- functions/ENGINE/checkin/COMPONENT/button/ready.js
+++ functions/ENGINE/checkin/COMPONENT/button/ready.js
@@ -21,10 +21,8 @@ module.exports.run = async (interaction) => {
   // ping team, once
   const messages = await interaction.channel.messages.fetch();
   const botMessages = messages.filter((message) => message.author.id === client.user.id);
-  if (botMessages.length >= 1) return;
-  console.log(botMessages);
-  console.log(botMessages.length);
-  // interaction.channel.send(`<@&${config.teamRole}>`);
+  if (botMessages.size >= 2) return;
+  interaction.channel.send(`<@&${config.teamRole}>`);
 
   // gray out button
   interaction.message.edit({ components: [buttons] });
