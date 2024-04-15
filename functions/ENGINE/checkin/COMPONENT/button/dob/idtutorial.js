72aabd76d07bb8671a7fd96b0e7f988b8db677cf
Phil <philip.schaefer0@gmail.com>
2024-04-15 20:18:21 +0200

    changed function

--- functions/ENGINE/checkin/COMPONENT/button/dob/idtutorial.js
+++ functions/ENGINE/checkin/COMPONENT/button/dob/idtutorial.js
@@ -4,7 +4,7 @@ module.exports.run = async (interaction) => {
 
   if (interaction.channel.parentId !== config.checkin.categoryID) return messageFail(interaction, 'This channel is not a check-in channel.');
 
-  interaction.channel.send(`Hey, <@${interaction.channel.name}> seems like you are not following the ID verification instructions correctly. Please have another read of <https://discord.com/channels/300051375914483715/496948681656893440/496985167160672267> and try again ^^`);
+  reply(interaction, `Hey, <@${interaction.channel.name}> seems like you are not following the ID verification instructions correctly. Please have another read of <https://discord.com/channels/300051375914483715/496948681656893440/496985167160672267> and try again ^^`);
 };
 
 module.exports.data = {
