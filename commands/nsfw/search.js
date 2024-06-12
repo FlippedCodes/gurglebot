d0d840f74a32f53f72d9b2409f52131146b9ca5c
Phil <philip.schaefer0@gmail.com>
2024-06-13 01:20:15 +0200

    fix no serverID logged

--- commands/nsfw/search.js
+++ commands/nsfw/search.js
@@ -37,7 +37,7 @@ module.exports.run = async (interaction, moment, EmbedBuilder) => {
   // get user tags and format dates
   const teammember = await client.users.fetch(DBentry.teammemberID);
   const teammemberTag = teammember ? teammember.tag : 'none';
-  const serverName = client.guilds.cache.find(({ id }) => id === DBentry.serverID).name;
+  const serverName = DBentry.serverID ? client.guilds.cache.find(({ id }) => id === DBentry.serverID).name : 'unknown';
   const [updatedAt, createdAt] = [DBentry.updatedAt, DBentry.createdAt].map((date) => moment(date).format('ddd, MMM Do YYYY, h:mm a'));
   // send it
   sendMessage(EmbedBuilder, interaction, user.tag, userID, DBentry.allow, teammemberTag, serverName, updatedAt, createdAt);
