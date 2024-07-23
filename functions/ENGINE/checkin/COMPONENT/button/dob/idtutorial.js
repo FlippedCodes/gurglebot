03be29ea1c7a42c8615db761a61f801f28b65700
Phil <philip.schaefer0@gmail.com>
2024-07-23 12:24:41 +0200

    add user mention

--- functions/ENGINE/checkin/COMPONENT/button/dob/idtutorial.js
+++ functions/ENGINE/checkin/COMPONENT/button/dob/idtutorial.js
@@ -5,7 +5,7 @@ module.exports.run = async (interaction) => {
   // remvove user reaction
   const channel = await interaction.guild.channels.cache.get(config.checkin.reaction.channel);
   const message = await channel.messages.fetch(config.checkin.reaction.message);
-  const content = `# ${message.content.split('# ')[2]}`;
+  const content = `<@${interaction.channel.name}>\n\n# ${message.content.split('# ')[2]}`;
 
   interaction.channel.send(content);
   messageSuccess(interaction, 'Instructions sent.', undefined, true);
